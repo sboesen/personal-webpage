16b41544c0a6d7a52cf5a0ef73e41d0ae68d112b
Stefan Boesen <stefan.boesen@gmail.com>
2014-08-11 20:40:03 -0400

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+
+    /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
+    GitHubActivity.feed({ username: "sboesen", selector: "#ghfeed" });
+
+});
